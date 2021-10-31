503e6d187b06971832d56031566b510e0b67d956
HACKER_PHONE_VIP <86104063+claprusshow@users.noreply.github.com>
2021-10-31 20:21:18 +0500

    Add files via upload

---
+++ FTG WATCH.py
@@ -0,0 +1,63 @@
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.bum", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+       
+ 
+    await event.edit("🕛")
+    await asyncio.sleep(0.7)
+    await event.edit("🕐")
+    await asyncio.sleep(0.7)
+    await event.edit("🕑")
+    await asyncio.sleep(0.7)
+    await event.edit("🕒")
+    await asyncio.sleep(0.7)
+    await event.edit("🕓")
+    await asyncio.sleep(0.7)
+    await event.edit("🕔")
+    await asyncio.sleep(1)
+    await event.edit("🕕")
+    await asyncio.sleep(0.7)
+    await event.edit("🕖")
+    await asyncio.sleep(0.7)
+    await event.edit("🕗")
+    await asyncio.sleep(0.7)
+    await event.edit("🕘")
+    await asyncio.sleep(0.7)
+    await event.edit("🕙")
+    await asyncio.sleep(0.7)
+    await event.edit("🕚")
+    await asyncio.sleep(0.7)
+    await event.edit("🕛")
+ 
+    await event.edit("🕛")
+    await asyncio.sleep(0.7)
+    await event.edit("🕐")
+    await asyncio.sleep(0.7)
+    await event.edit("🕑")
+    await asyncio.sleep(0.7)
+    await event.edit("🕒")
+    await asyncio.sleep(0.7)
+    await event.edit("🕓")
+    await asyncio.sleep(0.7)
+    await event.edit("🕔")
+    await asyncio.sleep(1)
+    await event.edit("🕕")
+    await asyncio.sleep(0.7)
+    await event.edit("🕖")
+    await asyncio.sleep(0.7)
+    await event.edit("🕗")
+    await asyncio.sleep(0.7)
+    await event.edit("🕘")
+    await asyncio.sleep(0.7)
+    await event.edit("🕙")
+    await asyncio.sleep(0.7)
+    await event.edit("🕚")
+    await asyncio.sleep(0.7)
+    await event.edit("🕛")
